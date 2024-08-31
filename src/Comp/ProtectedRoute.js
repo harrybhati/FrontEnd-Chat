776ce2f7399e152fba61ce90134791500ab7ae64
Harendra Bhati <61133065+harrybhati@users.noreply.github.com>
2024-08-31 20:02:55 +0530

    Update ProtectedRoute.js

--- src/Comp/ProtectedRoute.js
+++ src/Comp/ProtectedRoute.js
@@ -5,7 +5,10 @@ function ProtectedRoute(){
     // protected route means if user is login or singup then user can go further routes other wise not 
     const auth=localStorage.getItem('user');
     // if data store in local store is true then user can move further routes otherwise not 
-    return auth ?<Outlet/>:<Navigate to='/'></Navigate>
+    // return auth ?<Outlet/>:<Navigate to='/'></Navigate>
+        if (!auth) {
+  return <Redirect to="/login" />;
+}
     return(<>
     
     
