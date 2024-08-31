c320e6a63ce3acc3175d4036dd0adc17810da9d9
Harendra Bhati <61133065+harrybhati@users.noreply.github.com>
2024-08-31 20:00:20 +0530

    Update SingIn.js

--- src/Comp/SingIn.js
+++ src/Comp/SingIn.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import './SingIn.css';
